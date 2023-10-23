bee52d1a8e3e1276bdbe418c673ac33fe1edc656
abernatskiy <abernatskiy@toha.li>
2023-10-23 17:22:47 +0900

    Complete migration with issues
    The squid cannot sync: archive 503s every time.
    
    	modified:   commands.json
    	modified:   docker-compose.yml
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   schema.graphql
    	modified:   squid.yaml
    	modified:   src/abi/erc20.ts
    	new file:   src/main.ts
    	modified:   src/model/generated/marshal.ts
    	modified:   src/model/generated/owner.model.ts
    	modified:   src/model/generated/transfer.model.ts
    	modified:   src/model/index.ts
    	deleted:    src/model/marshal.ts
    	deleted:    src/model/owner.model.ts
    	deleted:    src/model/transfer.model.ts
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -1,115 +1,51 @@
-import { lookupArchive } from "@subsquid/archive-registry"
-import * as ss58 from "@subsquid/ss58"
-import {toHex} from "@subsquid/util-internal-hex"
-import {BatchContext, BatchProcessorItem, SubstrateBatchProcessor} from "@subsquid/substrate-processor"
-import {Store, TypeormDatabase} from "@subsquid/typeorm-store"
-import {In} from "typeorm"
-import * as erc20 from "./abi/erc20"
-import {Owner, Transfer} from "./model"
- 
+import {toHex} from '@subsquid/util-internal-hex'
+import * as ss58 from '@subsquid/ss58'
+import {lookupArchive} from '@subsquid/archive-registry'
+import {
+    BlockHeader,
+    DataHandlerContext,
+    SubstrateBatchProcessor,
+    SubstrateBatchProcessorFields,
+    Event as _Event,
+    Call as _Call,
+    Extrinsic as _Extrinsic
+} from '@subsquid/substrate-processor'
+
+export const SS58_NETWORK = 'astar' // used for the ss58 prefix, astar shares it with shibuya
 
 const CONTRACT_ADDRESS_SS58 = 'XnrLUQucQvzp5kaaWLG9Q3LbZw5DPwpGn69B5YcywSWVr5w'
-const CONTRACT_ADDRESS = toHex(ss58.decode(CONTRACT_ADDRESS_SS58).bytes)
-const SS58_PREFIX = ss58.decode(CONTRACT_ADDRESS_SS58).prefix
- 
- 
-const processor = new SubstrateBatchProcessor()
+export const CONTRACT_ADDRESS = ss58.codec(SS58_NETWORK).decode(CONTRACT_ADDRESS_SS58)
+
+console.log('CONTRACT_ADDRESS', CONTRACT_ADDRESS)
+
+export const processor = new SubstrateBatchProcessor()
     .setDataSource({
-        archive: lookupArchive("shibuya", { release: "FireSquid" })
-    })
-    .addContractsContractEmitted(CONTRACT_ADDRESS, {
-        data: {
-            event: {args: true}
-        }
-    } as const)
- 
- 
-type Item = BatchProcessorItem<typeof processor>
-type Ctx = BatchContext<Store, Item>
- 
- 
-processor.run(new TypeormDatabase(), async ctx => {
-    const txs = extractTransferRecords(ctx)
- 
-    const ownerIds = new Set<string>()
-    txs.forEach(tx => {
-        if (tx.from) {
-            ownerIds.add(tx.from)
-        }
-        if (tx.to) {
-            ownerIds.add(tx.to)
+        // Lookup archive by the network name in Subsquid registry
+        // See https://docs.subsquid.io/substrate-indexing/supported-networks/
+        archive: lookupArchive('shibuya', {release: 'ArrowSquid'}),
+        // Chain RPC endpoint is required on Substrate
+        chain: {
+            // See https://docs.subsquid.io/substrate-indexing/setup/general/#set-data-source
+            url: 'https://shibuya.public.blastapi.io',
+            rateLimit: 10
         }
     })
- 
-    const ownersMap = await ctx.store.findBy(Owner, {
-        id: In([...ownerIds])
-    }).then(owners => {
-        return new Map(owners.map(owner => [owner.id, owner]))
+    .addContractsContractEmitted({
+        contractAddress: [CONTRACT_ADDRESS],
+        extrinsic: true
     })
- 
-    const transfers = txs.map(tx => {
-        const transfer = new Transfer({
-            id: tx.id,
-            amount: tx.amount,
-            block: tx.block,
-            timestamp: tx.timestamp
-        })
- 
-        if (tx.from) {
-            transfer.from = ownersMap.get(tx.from)
-            if (transfer.from == null) {
-                transfer.from = new Owner({id: tx.from, balance: 0n})
-                ownersMap.set(tx.from, transfer.from)
-            }
-            transfer.from.balance -= tx.amount
+    .setFields({
+        block: {
+            timestamp: true
+        },
+        extrinsic: {
+            hash: true
         }
- 
-        if (tx.to) {
-            transfer.to = ownersMap.get(tx.to)
-            if (transfer.to == null) {
-                transfer.to = new Owner({id: tx.to, balance: 0n})
-                ownersMap.set(tx.to, transfer.to)
-            }
-            transfer.to.balance += tx.amount
-        }
- 
-        return transfer
     })
- 
-    await ctx.store.save([...ownersMap.values()])
-    await ctx.store.insert(transfers)
-})
- 
- 
-interface TransferRecord {
-    id: string
-    from?: string
-    to?: string
-    amount: bigint
-    block: number
-    timestamp: Date
-}
- 
- 
-function extractTransferRecords(ctx: Ctx): TransferRecord[] {
-    const records: TransferRecord[] = []
-    for (const block of ctx.blocks) {
-        for (const item of block.items) {
-            if (item.name === 'Contracts.ContractEmitted' && item.event.args.contract === CONTRACT_ADDRESS) {
-                const event = erc20.decodeEvent(item.event.args.data)
-                if (event.__kind === 'Transfer') {
-                    records.push({
-                        id: item.event.id,
-                        from: event.from && ss58.codec(SS58_PREFIX).encode(event.from),
-                        to: event.to && ss58.codec(SS58_PREFIX).encode(event.to),
-                        amount: event.value,
-                        block: block.header.height,
-                        timestamp: new Date(block.header.timestamp)
-                    })
-                }
-            }
-        }
-    }
-    return records
-}
- 
+
+export type Fields = SubstrateBatchProcessorFields<typeof processor>
+export type Block = BlockHeader<Fields>
+export type Event = _Event<Fields>
+export type Call = _Call<Fields>
+export type Extrinsic = _Extrinsic<Fields>
+export type ProcessorContext<Store> = DataHandlerContext<Store, Fields>
